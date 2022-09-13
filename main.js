52852913ed2baeedd247b66800ab3c11af38ac4e
Saragam <git@saragam.com.np>
2022-09-13 20:39:34 +0545

    js selector inside element itself

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
-const toggleBtn = document.querySelectorAll(".btn");
+// const toggleBtn = document.querySelectorAll(".btn");
 
-toggleBtn.forEach((b) => {
-  b.addEventListener("click", (event) => {
-    const question = event.currentTarget.parentElement.parentElement;
-    question.classList.toggle("show")
-  });
-});
-
-
-
+// toggleBtn.forEach((b) => {
+//   b.addEventListener("click", (event) => {
+//     const question = event.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show")
+//   });
+// });
 
 // selector inside the element itself
+
+const questionArray = document.querySelectorAll(".question");
+
+questionArray.forEach((qsn) => {
+  const toggleBtn = qsn.querySelector(".btn");
+  toggleBtn.addEventListener("click", () => {
+    questionArray.forEach((item) => {
+      if (item === qsn) {
+        item.classList.toggle("show");
+      }
+    });
+  });
+});
